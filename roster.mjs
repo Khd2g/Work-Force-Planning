1445d162b2048f076a371efaea98bdb30010ea09
Khd2g <haylea_dale@yahoo.com>
2026-07-14 21:11:30 -0500

    Add files via upload

---
+++ roster.mjs
@@ -0,0 +1,9 @@
+{
+  "name": "roster-tracker",
+  "version": "1.0.0",
+  "private": true,
+  "description": "Live workforce roster tracker with new/departed staff detection, leave flagging, and hiring/ratio alerts.",
+  "dependencies": {
+    "@netlify/blobs": "^8.1.0"
+  }
+}
